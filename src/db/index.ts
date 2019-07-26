4cbe24e3d64e6605d31d7aed4bf902c83bd798d3
egorsmth <mattspring0@gmail.com>
2019-07-26 18:20:31 +0300

    fix4

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,5 +2,8 @@ import {createConnection} from "typeorm";
 
 export default createConnection({
     type: 'postgres',
-    url: process.env.DATABASE_URL
+    url: process.env.DATABASE_URL,
+    entities: [
+        __dirname + "entity/Point.js"
+    ]
 })
