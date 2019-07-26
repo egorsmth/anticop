d4b78bcc8c0e3f3a3998fa9bd64e22014ee40e25
egorsmth <mattspring0@gmail.com>
2019-07-26 18:36:32 +0300

    fix le finale

--- src/services/points/routes.ts
+++ src/services/points/routes.ts
@@ -22,18 +22,18 @@ export default [
         const lon: number = req.body.lon;
         const lat: number = req.body.lat;
 
-        const p = new Point(lat, lon);
-        const connection = await db;
-        await connection.manager.save(p);
-        console.log("Saved a new point with id: " + p.id);
+        // const p = new Point(lat, lon);
+        // const connection = await db;
+        // await connection.manager.save(p);
+        // console.log("Saved a new point with id: " + p.id);
 
-        console.log("Loading users from the database...");
-        const users = await connection.manager.find(Point);
-        console.log("Loaded users: ", users);
+        // console.log("Loading users from the database...");
+        // const users = await connection.manager.find(Point);
+        // console.log("Loaded users: ", users);
 
-        console.log("Here you can setup and run express/koa/any other framework.");
+        // console.log("Here you can setup and run express/koa/any other framework.");
 
-        res.send(`alert saved (lon:${lon}; lat:${lat}); all ${users}`)
+        res.send(`alert saved (lon:${lon}; lat:${lat})`)
     }    
   }
 ];
